0d84f60afeade90cb46656d0d90b077ba927d6c0
HenriqueMVSS <henrique_mateus1994@hotmail.com>
2024-12-24 06:01:00 -0300

    doc: Implemented Swagger.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,41 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('Autenticação')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({ summary: 'Cadastre um novo usuário' })
+  @ApiResponse({ status: 201, description: 'Usuário cadastrado com sucesso.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string', example: 'user1', description: 'O nome de usuário do usuário' },
+        password: { type: 'string', example: 'password1', description: 'A senha do usuário' },
+      },
+    },
+  })
   @Post('signup')
   async signUp(@Body('username') username: string, @Body('password') password: string) {
     return await this.authService.signUp(username, password);
   }
 
+  @ApiOperation({ summary: 'Login do usuário' })
+  @ApiResponse({ status: 200, description: 'Usuário logado com sucesso.' })
+  @ApiResponse({ status: 401, description: 'Não autorizado.' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string', example: 'user1', description: 'O nome de usuário do usuário' },
+        password: { type: 'string', example: 'password1', description: 'A senha do usuário' },
+      },
+    },
+  })
   @Post('signin')
   async signIn(@Body('username') username: string, @Body('password') password: string) {
     return this.authService.signIn(username, password);
--- src/transactions/transactions.controller.ts
+++ src/transactions/transactions.controller.ts
@@ -1,34 +1,90 @@
-import { Controller, Post, Body, Param, UseGuards, Request, Get } from '@nestjs/common';
+import { Controller, Post, Body, Param, UseGuards, Request, Get, HttpException, HttpStatus } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('Transações')
+@ApiBearerAuth()
 @Controller('transactions')
 export class TransactionsController {
   constructor(private readonly transactionsService: TransactionsService) {}
 
+  @ApiOperation({ summary: 'Saldo do usuário' })
+  @ApiResponse({ status: 200, description: 'Saldo visualizado com sucesso.' })
+  @ApiResponse({ status: 401, description: 'Não autorizado.' })
+  @UseGuards(JwtAuthGuard)
   @UseGuards(JwtAuthGuard)
   @Get('balance/:userId')
   async balance(@Request() req, @Param('userId') userId: number) {
     const loggedInUserId = req.user.userId;
     return await this.transactionsService.balance(loggedInUserId, userId);
   }
+
+  @ApiOperation({ summary: 'Deposito' })
+  @ApiResponse({ status: 201, description: 'Depósito realizado com sucesso.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        amount: { type: 'number', example: 100, description: 'O valor a depositar' },
+      },
+    },
+  })
   @UseGuards(JwtAuthGuard)
   @Post('deposit/:userId')
   async deposit(@Param('userId') userId: number, @Body('amount') amount: number) {
-    return await this.transactionsService.deposit(userId, amount);
+    try {
+      await this.transactionsService.deposit(userId, amount);
+      return { message: `Depósito de R$ ${amount} realizado com sucesso.` };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
+  @ApiOperation({ summary: 'Saque' })
+  @ApiResponse({ status: 201, description: 'Saque realizado com sucesso' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        amount: { type: 'number', example: 100, description: 'The amount to withdraw' },
+      },
+    },
+  })
   @UseGuards(JwtAuthGuard)
   @Post('withdraw/:userId')
   async withdraw(@Request() req, @Param('userId') userId: number, @Body('amount') amount: number) {
     const loggedInUserId = req.user.userId;
-    return this.transactionsService.withdraw(loggedInUserId, userId, amount);
+    try {
+      await this.transactionsService.withdraw(loggedInUserId, userId, amount);
+      return { message: `Saque de R$ ${amount} realizado com sucesso.` };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 
+  @ApiOperation({ summary: 'Transferência' })
+  @ApiResponse({ status: 201, description: 'Transferência solicitada com sucesso.' })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        amount: { type: 'number', example: 100, description: 'O valor a ser transferido' },
+      },
+    },
+  })
   @UseGuards(JwtAuthGuard)
   @Post('transfer/:fromUserId/:toUserId')
   async transfer(@Request() req, @Param('fromUserId') fromUserId: number, @Param('toUserId') toUserId: number, @Body('amount') amount: number) {
     const loggedInUserId = req.user.userId;
-    return this.transactionsService.transfer(loggedInUserId, fromUserId, toUserId, amount);
+    try {
+      await this.transactionsService.transfer(loggedInUserId, fromUserId, toUserId, amount);
+      return { message: `Transferência de R$ ${amount} realizada com sucesso.` };
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
